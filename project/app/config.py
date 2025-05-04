d381873d5992aa1dad774a27960ae26d4e2ae25e
Everton França <48794204+EvertonFSilva@users.noreply.github.com>
2025-05-04 19:56:04 -0300

    Delete project directory

--- project/app/config.py
+++
@@ -1,3 +0,0 @@
-DB_PATH = "C:/Users/evert/Desktop/Server/dados.db"
-MODEL_NAME = "llama3-70b-8192"
-API_KEY = "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"
--- project/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-from app.views.routes import router as websocket_router
-
-app = FastAPI()
-app.include_router(websocket_router)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", use_colors=False)
